64b36ac9cbc8a7442e7b4d060cb3c1668c951e44
mazzu <denismazzu@gmail.com>
2018-12-01 14:48:55 +0100

    Undef is equal to Undef

--- src/State.hs
+++ src/State.hs
@@ -6,29 +6,30 @@ module State
     )
 where
 
-data State = Undef 
+data State = Undef
            | Def [Entry]
            deriving (Show,Read)
 
 type Entry = (String, Integer)
 
 instance Eq State where
-    (Def entries1) == (Def entries2) = 
+    (Def entries1) == (Def entries2) =
         entries1 `is_contained` entries2 && entries2 `is_contained` entries1
+    Undef == Undef = True
     _ == _ = False
 
-state :: [Entry] -> State    
-state entries = Def entries 
+state :: [Entry] -> State
+state entries = Def entries
 
 is_contained :: [Entry] -> [Entry] -> Bool
 is_contained [] _ = True
-is_contained (first_entry:other_entries) entries2 = 
+is_contained (first_entry:other_entries) entries2 =
     (entries2 `contains` first_entry) && (other_entries `is_contained` entries2)
 
 contains :: [Entry] -> Entry -> Bool
-contains []      entry = False 
+contains []      entry = False
 contains (first_entry:other_entries) entry =
-    (first_entry == entry) || (other_entries `contains` entry) 
+    (first_entry == entry) || (other_entries `contains` entry)
 
 bottom :: State -> State
 bottom = \s -> Undef
