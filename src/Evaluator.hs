e8b40e06722304405574b91d66775824f8fb5a6d
Francesco Parolini <parolini.francesco.1@gmail.com>
2018-12-01 18:09:03 +0100

    better names

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -18,37 +18,31 @@ import UpdateState
 -------------------------------------------------------------------------------
 
 semantics :: Stmt -> State -> State
-
 semantics (Assign identifier aexpr) = update_state identifier aexpr
-    
 semantics Skip = id
-
 semantics (Seq s1 s2) = 
     (semantics s2) . (semantics s1)
-
 semantics (If b s1 s2) =
     cond ((eval_bexpr b), (semantics s1), (semantics s2))
-
 semantics (While b s) = fix f
     where f = \g -> cond (eval_bexpr b, g . semantics s, id)
 
-fix :: ((State -> State) -> (State -> State)) -> State -> State
-fix f = lub [ fnth f n bottom | n <- [0..] ] 
-
-fnth :: 
-  ((State -> State) -> (State -> State)) -> 
-  Int -> 
-  (State -> State) ->
-  State -> State
-fnth f 0 = id
-fnth f n = f . (fnth f (n-1))
-  
-lub :: [(State -> State)] -> State -> State
-lub (g:gs) s 
-    | (g s) /= Undef = g s -- if exist g (and g s) is unique, also g is the least
-    | otherwise = lub gs s
-  
 cond :: (State -> Bool, State -> State, State -> State) -> State -> State
 cond (p, g1, g2) s
-  | p s == True  = g1 s
-  | p s == False = g2 s
+    | p s == True  = g1 s
+    | p s == False = g2 s
+
+-- Knaster–Tarski
+
+fix :: ((State -> State) -> (State -> State)) -> State -> State
+fix f = lub [ fnth f n bottom  | n <- [0..] ] 
+
+fnth :: ((State -> State) -> (State -> State)) -> Int -> (State -> State) -> State -> State
+fnth f n = (foldr (.) id (replicate n f))
+  
+lub :: [(State -> State)] -> State -> State
+lub (f:fs) s 
+    -- TODO: remove comment
+    | s' /= Undef = s' -- if exist g (and g s) is unique, also g is the least
+    | otherwise   = lub fs s
+    where s' = f s
