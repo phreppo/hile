b2f0a23091933d7f0c015d1e34eeb26d212a1ab3
phreppo <parolini.francesco.1@gmail.com>
2018-12-12 09:40:00 +0100

    module calculator

--- src/WhilePrograms.hs
+++ src/WhilePrograms.hs
@@ -3,6 +3,7 @@ module WhilePrograms
         factorial,
         expon,
         fibonacci,
+        modul,
         empty_state
     )
 where
@@ -17,6 +18,9 @@ expon = "accumulator := 1; for i:=1 to y do accumulator:=accumulator*x"
 
 -- fib(x). fn1 = fib(n-1), fn2 = fib(n-2)
 fibonacci = "fn1 := 1; fn2 := 0; for i:=2 to x do (fn := fn1+fn2; fn2 := fn1; fn1 := fn)"
+
+-- x % y
+modul = "r := x; while r >= y do r := r - y "
 
 empty_state = "[]"
 
